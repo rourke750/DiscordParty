60dc31c1b948cd1622ac0560a1d24bc3b4952af7
Rourke750 <rourke7501@gmail.com>
2023-10-21 23:02:07 -0400

    run in try catch

--- discordparty/events/random_events.py
+++ discordparty/events/random_events.py
@@ -9,19 +9,22 @@ users = []
 
 @tasks.loop(seconds=10)
 async def random_event_movement_channel():
-    for t in list(users):
-        user_id = t[0]
-        guild_id = t[1]
-        guild = bot.get_guild(guild_id)
-        user = guild.get_member(user_id)
-        if not user.voice:
-            # user is no longer in voice remove them
-            users.remove(t)
-            logging.info('removing user as they are no longer in voice')
-            continue
-            
-        voice = utils.get_random_voice(guild)
-        await user.move_to(voice)
+    try:
+        for t in list(users):
+            user_id = t[0]
+            guild_id = t[1]
+            guild = bot.get_guild(guild_id)
+            user = guild.get_member(user_id)
+            if not user.voice:
+                # user is no longer in voice remove them
+                users.remove(t)
+                logging.info('removing user as they are no longer in voice')
+                continue
+                
+            voice = utils.get_random_voice(guild)
+            await user.move_to(voice)
+    except Exception as e:
+        logging.error(e)
     
 def add_user(user_id, guild_id):
     users.append((user_id, guild_id,))
